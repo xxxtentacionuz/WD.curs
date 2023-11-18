1171e23097c241a332a7088464107fb96f0fa6e4
xxxtentacionuz <samariddinxacker@gmail.com>
2023-11-18 16:59:19 +0500

    bot

--- backend/controllers/TestbotController.php
+++ backend/controllers/TestbotController.php
@@ -8,30 +8,6 @@ use yii\web\Controller;
 
 class TestbotController extends Controller
 {
-
-    public function actionSend()
-    {
-        $text = 'index/send';
-        $chat_id = 844272310;
-
-        $data = [
-            'chat_id' => $chat_id,
-            'text' => $text,
-
-            ];
-
-
-        $token = "6816596578:AAGcblyvf4sGmrvMY3iPf4TCou2nwnMiQFI";
-        $url = 'https://api.telegram.org/bot'.$token.'/sendMessage?'.http_build_query($data);
-        $curl = curl_init($url);
-//        curl_setopt($curl, CURLOPT_POST, true);
-//        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        $response = curl_exec($curl);
-        curl_close($curl);
-        return $response;
-    }
-
     public $enableCsrfValidation = false;
 
     public function actionWebhook()
@@ -41,7 +17,12 @@ class TestbotController extends Controller
 
         $chat_id = $data['message']['from']['id'];
         $text = isset($data['message']['text']) ? $data['message']['text']: '';
+        $this->sendMessage($chat_id, $text);
 
+    }
+
+    public function sendMessage($chat_id, $text)
+    {
         $data = [
             'chat_id' => $chat_id,
             'text' => $text,
